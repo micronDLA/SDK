a4d30549d1ae4fddc680eeebda877cec01470324
lburzawa <lburzawa@purdue.edu>
2018-08-14 16:27:25 -0400

    Update install.sh

--- sdk/install.sh
+++ sdk/install.sh
@@ -10,51 +10,16 @@ then
 	then
 		apt-get update
 		apt-get -y install python3-pip
-		apt-get -y install git
-		if [ `lsb_release --release | cut -f2 | cut -d. -f1` -lt '16' ]
-		then
-			cp libsnowflake-gcc4.8.so /usr/local/lib
-			ln -s libsnowflake-gcc4.8.so libsnowflake.so
-			# Protobuf
-			echo "Installing protobuf from source"
-			cd /tmp
-			wget https://github.com/google/protobuf/releases/download/v3.5.1/protobuf-all-3.5.1.tar.gz
-			tar xf protobuf-all-3.5.1.tar.gz
-			cd protobuf-3.5.1
-			./configure
-			make
-			make install
-
-			# Pytorch
-			apt-get -y install zlib1g-dev
-			apt-get -y install libjpeg-dev
-			apt-get -y install libyaml-dev
-			pip3 install pyyaml
-			pip3 install --upgrade numpy
-			pip3 install Pillow
-			cd /tmp
-			wget https://cmake.org/files/v3.10/cmake-3.10.2-Linux-x86_64.tar.gz
-			tar xf cmake-3.10.2-Linux-x86_64.tar.gz
-			cp -r cmake-3.10.2-Linux-x86_64/* /usr/local/
-			git clone --recursive https://github.com/pytorch/pytorch
-			cd pytorch
-			python3 setup.py install
-		else
-			cp libsnowflake-gcc5.4.so /usr/local/lib
-			ln -s libsnowflake-gcc5.4.so libsnowflake.so
-			PYVERSION=`python3 --version|cut -d. -f2`
-			if [ $PYVERSION == 6 ]
-			then
-				pip3 install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp36-cp36m-linux_x86_64.whl
-				pip3 install --upgrade torchvision
-			elif [ $PYVERSION == 5 ]
-			then
-				pip3 install http://download.pytorch.org/whl/cpu/torch-0.4.0-cp35-cp35m-linux_x86_64.whl
-				pip3 install --upgrade torchvision
-			else
-				echo 'Unknown python version, torch will not be installed'
-			fi
-		fi
+		pip3 install --upgrade numpy
+		# Protobuf
+		echo "Installing protobuf from source"
+		cd /tmp
+		wget https://github.com/google/protobuf/releases/download/v3.6.1/protobuf-all-3.6.1.tar.gz
+		tar xf protobuf-all-3.6.1.tar.gz
+		cd protobuf-3.6.1
+		./configure
+		make -j4
+		make install
 		echo "Installing thnets from source"
 		cd /tmp
 		git clone https://github.com/mvitez/thnets
